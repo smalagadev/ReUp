ac6575a50e597ca19ed148a369924264c1ae1ba4
Steffano <smalagadev@gmail.com>
2020-09-02 02:26:08 -0400

    Components added and remainding boilerplate code removed

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
 import Schedule from './components/Schedule';
+import Dashboard from './components/Dashboard';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <Schedule />
-      <header className="App-header">
-
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Check your schedule
-        </a>
-      </header>
-    </div>
+    <>
+      <Dashboard />
+      <Schedule />
+    </>
   );
 }
 
