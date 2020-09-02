2052221a53477a1f97bd046131a68498dfc27a46
Steffano <smalagadev@gmail.com>
2020-09-01 23:32:30 -0400

    src folder name changed to client

---
+++ client/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import Schedule from './components/Schedule';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+    <Schedule />
+      <header className="App-header">
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Check your schedule
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Schedule/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import WorkDay from "./../WorkDay";
+
+class Schedule extends Component{
+  render(){
+    return(
+      <>
+        <h1>This is a schedule</h1>
+        <WorkDay />
+      </>
+    );
+  }
+}
+
+export default Schedule;
