47ef230516b6cded4658b207f66dba9229f7e475
Steffano <smalagadev@gmail.com>
2020-09-05 17:24:17 -0400

    Pages added

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import 'materialize-css/dist/css/materialize.min.css';
-import Schedule from './components/Schedule';
 import Dashboard from './components/Dashboard';
+import Calendar from './pages/Calendar';
+import ChangeAvailability from './pages/ChangeAvailability';
+import RequestTimeOff from './pages/RequestTimeOff';
+import Login from './pages/Login';
+import Register from './pages/Register';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Dashboard />
-      <Schedule />
+      <Router>
+        <Dashboard />
+        <Switch>
+          <Route exact path='/Login' component={Login}/>
+          <Route exact path='/Register' component={Register}/>
+          <Route exact path='/Calendar' component={Calendar}/>
+          <Route exact path='/RequestTimeOff' component={RequestTimeOff}/>
+          <Route exact path='/ChangeAvailability' component={ChangeAvailability}/>
+        </Switch>
+      </Router>
     </>
   );
 }
